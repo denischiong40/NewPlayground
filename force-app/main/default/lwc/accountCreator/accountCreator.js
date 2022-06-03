779c52572072c1dc7d88b6634b56e6e78544151a
Denis Chiong <denischiong40@gmail.com>
2022-06-03 00:47:15 -0400

    Playground doing badges

--- force-app/main/default/lwc/accountCreator/accountCreator.js
+++ force-app/main/default/lwc/accountCreator/accountCreator.js
@@ -4,9 +4,11 @@ import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import First_Name__c from '@salesforce/schema/Account.First_Name__c';
+import Last_Name__c from '@salesforce/schema/Account.Last_Name__c';
 export default class AccountCreator extends LightningElement {
   objectApiName = ACCOUNT_OBJECT;
-  fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
+  fields = [First_Name__c, Last_Name__c, NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
   handleSuccess(event) {
     const toastEvent = new ShowToastEvent({
       title: 'Account created',
--- force-app/main/default/lwc/clock/clock.js
+++ force-app/main/default/lwc/clock/clock.js
@@ -1,10 +1,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Clock extends LightningElement {
-    timestamp = new Date();
+  timestamp = new Date();
 
-    @api
-    refresh() {
-        this.timestamp = new Date();
-    }
+  @api
+  refresh() {
+    this.timestamp = new Date();
+  }
+  connectedCallback() {
+    this.refresh();
+    setInterval(() => {
+      this.refresh();
+    }, 1000);
+  }
 }
